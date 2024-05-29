47ace0f1ed4a6dfb3338f0965a25d7003856a196
FerlinFT12 <ferlinFT@gmail.com>
2024-05-29 13:09:12 +0700

    update layout pesanan bundling

--- app/Http/Controllers/Pos/PosController.php
+++ app/Http/Controllers/Pos/PosController.php
@@ -326,7 +326,7 @@ class PosController extends Controller
             $li .= '</ul>';
 
             $html = '   <div class="col-12 col-md-6 col-lg-4 item">
-                            <div class="card m-0">
+                            <div class="card">
                                 <div class="card-body item-body" style="background-color: #F2F2F280">
                                     <div class="row">
                                         <div class="col-lg-4 col-md-4 col-3">
